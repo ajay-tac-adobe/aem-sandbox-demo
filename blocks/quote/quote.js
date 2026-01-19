1fd8804bca6b6e7c431992993f6bd617c7dfa800
ajay-tac-adobe <tacadobe4@gmail.com>
2026-01-19 12:07:34 +0530

    added quote suffix

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,21 @@
-export default function decorate(block) {
+import { fetchPlaceholders } from '../../scripts/placeholders.js';
+
+export default async function decorate(block) {
+  const placeholders = await fetchPlaceholders();
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+   // Create suffix element
+  const suffixText = placeholders['quote-of-the-day'];
+  if (suffixText) {
+    const suffix = document.createElement('div');
+    suffix.className = 'quote-suffix';
+    suffix.textContent = suffixText;
+
+    // Replace content with quote + suffix
+    quoteWrapper.replaceChildren(blockquote, suffix);
+  } else {
+    quoteWrapper.replaceChildren(blockquote);
+  }
 }
